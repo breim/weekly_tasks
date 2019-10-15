1b8d5e0ff29bfe04cb21378a514febec61fd2bdb
Henrique <henrique@breim.com.br>
2019-10-15 15:34:05 -0300

    Fix current redirect after devise login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,10 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user
   protect_from_forgery with: :null_session
+
+	private
+
+	def after_sign_in_path_for(_resource)
+	 	dashboard_tasks_path
+  end
 end
